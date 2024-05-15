80ebab58637ba4b693a872fd1234e5003690a935
Mawusi Kpodo <60935336+mawusikpodo@users.noreply.github.com>
2024-05-15 19:29:33 +0000

    backend

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response} from "express";
+import User from "../models/user";
+
+const router = express.Router()
+
+router.post("/register", async(req: Request, res: Response)=> {
+    try{
+       let user = await User.findOne({
+           email: req.body.email,
+        });
+
+        if(user) {
+           return res.status(400).json({message: "User already exist"}) 
+        }
+
+        user = new User(req.body)
+        await user.save()
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message: "Something went wrong"})
+    }
+})
+
+export default router
